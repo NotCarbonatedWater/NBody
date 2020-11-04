d47b32dc58d198c64c37a5891b366908f89a8100
Jonathan Jaco <jacojonathan12@gmail.com>
2020-11-03 23:24:57 -0800

    added jframe and basic physics

--- NBody.java
+++ NBody.java
@@ -1,5 +1,3 @@
-
-// import java.io.File;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -7,10 +5,10 @@ import java.util.Scanner;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.Graphics;
 
 class ArrayList<E> {
@@ -56,10 +54,7 @@ class ArrayList<E> {
     }
 
     boolean checkEmpty() {
-        if (size == 0)
-            return true;
-        else
-            return false;
+        return (size == 0);
     }
 
     int currentLen() {
@@ -80,66 +75,93 @@ public class NBody extends JPanel implements ActionListener {
 
     Timer tm = new Timer(5, this);
     int x = 0, velX = 2;
+    static int NUM_OF_BODIES = 0;
+    static int FRAME_WIDTH = 768;
+    static int FRAME_HEIGHT = 768;
+    static ArrayList<String> A1 = new ArrayList<String>();
 
+    @Override // Signature Visual Code Error //
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(Color.RED);
-        g.drawRect(100, 10, 30, 40);
-        g.fillRect(10, 10, 20, 10);
 
-        g.setColor(Color.BLUE);
-        g.fillOval(x, 100, 50, 50);
+        for (int i = 0; i < NUM_OF_BODIES; i++) {
+            int tmpx = (int) Float.parseFloat(A1.get(2 + (i * 7)));
+            int tmpy = (int) Float.parseFloat(A1.get(3 + (i * 7)));
+            int bodySize = (int) Float.parseFloat(A1.get(6 + (i * 7)));
+            g.fillOval(tmpx, tmpy, bodySize, bodySize);
+        }
 
         tm.start();
     }
 
+    public static void runPhyics() {
+        for (int i = 0; i < NUM_OF_BODIES; i++) {
+            Float tmpx = Float.parseFloat(A1.get(2 + (i * 7)));
+            Float tmpy = Float.parseFloat(A1.get(3 + (i * 7)));
+            Float tmpVx = Float.parseFloat(A1.get(4 + (i * 7)));
+            Float tmpVy = Float.parseFloat(A1.get(5 + (i * 7)));
+
+            A1.replace(2 + (i * 7), Float.toString(tmpx + tmpVx));
+            A1.replace(3 + (i * 7), Float.toString(tmpy + tmpVy));
+            A1.replace(4 + (i * 7), Float.toString(tmpVx));
+            A1.replace(5 + (i * 7), Float.toString(tmpVy));
+
+        }
+        System.out.println("ARRAY LIST IN PHYSICS");
+        for (int i = 0; i < A1.size(); i++)
+            System.out.print(A1.get(i) + "\n");
+    }
+
     public void actionPerformed(ActionEvent e) {
         x = x + velX;
+        runPhyics();
         repaint();
     }
 
-    public static void runWithArrayList(float scale, Scanner fileIn) {
+    public static void runFrame() {
+        // Graphics Set up //
+        NBody n = new NBody();
+        JFrame jf = new JFrame();
+        jf.setTitle("NBody");
+        jf.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        jf.setVisible(true);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.add(n);
+    }
 
-        ArrayList<String> A1 = new ArrayList<String>();
+    public static void runWithArrayList(float scale, Scanner fileIn) {
+        runFrame();
 
         while (fileIn.hasNext()) {
             String tmp = fileIn.next();
             String[] commaRid = tmp.split(",");
-            for (int i = 0; i < commaRid.length; i++) {
+            for (int i = 0; i < commaRid.length; i++)
                 A1.add(commaRid[i]);
-            }
         }
         System.out.println("ARRAY LIST");
+        for (int i = 0; i < A1.size(); i++)
+            System.out.print(A1.get(i) + "\n");
+
+        System.out.println("ARRAY LIST");
+
         for (int i = 0; i < A1.size(); i++) {
             System.out.print(A1.get(i) + "\n");
         }
+        System.out.println("\n\n" + (A1.size / 7));
+        NUM_OF_BODIES = (A1.size / 7);
 
-        A1.replace(0, "696969696969696969696969");
-
-        System.out.println("ARRAY LIST");
-        for (int i = 0; i < A1.size(); i++) {
-            System.out.print(A1.get(i) + "\n");
-        }
     }
 
     public static void main(String[] argv) throws FileNotFoundException {
-
         // file reader start//
         // loads file //
 
         // terminal switch: //
         // Scanner fileIn = new Scanner(new File(argv[0]));
-
         // VS CODE Switch: // nbody_input.txt
         File file = new File("nbody_input.txt");
         Scanner fileIn = new Scanner(file);
-
-        // fileIn.nextLine(); // Reads one line from the file
-        // fileIn.next(); // Reads one word from the file
-
-        // fileIn.hasNext(); // Returns true if there is another word in the file
-        // fileIn.hasNextLine(); // Returns true if there is another line to read from
-        // the file
 
         // reads data structure type //
         String dataStruct = fileIn.nextLine();
@@ -158,17 +180,7 @@ public class NBody extends JPanel implements ActionListener {
         } else {
             System.out.println("Error in txt file - found");
         }
-
         fileIn.close();
         // file reader end //
-
-        // Graphics Set up //
-        NBody n = new NBody();
-        JFrame jf = new JFrame();
-        jf.setTitle("NBody");
-        jf.setSize(768, 768);
-        jf.setVisible(true);
-        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jf.add(n);
     }
 }
